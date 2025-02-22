6150fa53b29083f8a38889b8a7cca12e7a8e2590
Heewon Ahn <57425238+shelost@users.noreply.github.com>
2025-02-22 17:34:05 -0500

    Initial fixes

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -12,9 +12,6 @@
 	</div>
 
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
 			<li aria-current={page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
@@ -26,9 +23,6 @@
 				<a href="/sverdle">Sverdle</a>
 			</li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
 	</nav>
 
 	<div class="corner">
@@ -38,10 +32,11 @@
 	</div>
 </header>
 
-<style>
+<style lang="scss">
 	header {
 		display: flex;
 		justify-content: space-between;
+		margin-top: 10px;
 	}
 
 	.corner {
@@ -69,27 +64,23 @@
 		--background: rgba(255, 255, 255, 0.7);
 	}
 
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
 	path {
 		fill: var(--background);
 	}
 
 	ul {
 		position: relative;
-		padding: 0;
+		padding: 0 24px;
 		margin: 0;
-		height: 3em;
+		height: 40px;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		list-style: none;
 		background: var(--background);
 		background-size: contain;
+		box-shadow: 0 15px 30px rgba(#030025, 0.12);
+		border-radius: 40px;
 	}
 
 	li {
